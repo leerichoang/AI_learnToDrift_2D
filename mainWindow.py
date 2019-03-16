b939e3b77e644f910f57c6388d2149419b015438
duydo <duy.ado97@gmail.com>
2019-03-16 01:07:37 -0700

    Update

--- mainWindow.py
+++ mainWindow.py
@@ -1,17 +1,23 @@
 from pyglet.gl import *
 from pyglet.window import key
 from pyglet.window import FPSDisplay
-
+import math 
 WINDOWWIDTH=1280
 WINDOWHEIGHT=720 
 
 class CarSprite():
     def __init__(self):
         self.carSprite_image = pyglet.image.load('car_sprite_resized10percent.png')
-        self.carSprite= pyglet.sprite.Sprite(self.carSprite_image, 500,500)
+        self.carSprite= pyglet.sprite.Sprite(self.carSprite_image, WINDOWWIDTH/2,WINDOWHEIGHT/2)
         self.x = self.carSprite.x
         self.y = self.carSprite.y
         self.theta = self.carSprite.rotation
+        self.speed = 200.0
+        self.rotation_speed = 200.0
+        self.deltaX = 0.0
+        self.deltaY = 0.0
+        print(self.carSprite.width)
+        print(self.carSprite.height)
     def draw(self):
         self.carSprite.draw()
     
@@ -25,6 +31,12 @@ class CarSprite():
         self.theta = self.carSprite.rotation
         return self.theta
 
+    def getDeltaX(self):
+        return self.deltaX
+    def getDeltaY(self):
+        return self.deltaY
+
+
     def updateX(self,value):
         value += self.getX()
         self.carSprite.update(x=value)
@@ -34,8 +46,10 @@ class CarSprite():
     def updateTheta(self,degree):  
         degree += self.getTheta()
         self.carSprite.update(rotation=degree)
-
-
+    def updateDeltaX(self, value):
+        self.deltaX = value
+    def updateDeltaY(self, value):
+        self.deltaY = value
     
 
 class MyWindow(pyglet.window.Window):
@@ -47,37 +61,6 @@ class MyWindow(pyglet.window.Window):
         print(self.car.theta)
         self.key_handler = key.KeyStateHandler()
 
-    # def on_key_press(self, symbol,modifiers):
-    #     if symbol == key.MOTION_UP:
-    #         print('accelerate')
-    #         self.car.updateX(20)
-    #     elif symbol == key.MOTION_DOWN:
-    #         print('reverse')
-    #         self.car.updateX(-20)
-    #     elif symbol == key.MOTION_LEFT:
-    #         print('turn left')
-    #         #self.car.updateY(20)
-    #         self.car.updateTheta(-2)
-    #     elif symbol == key.MOTION_RIGHT:
-    #         print('turn right')
-    #         #self.car.updateY(-20)
-    #         self.car.updateTheta(2)
-    #     elif symbol == key.ESCAPE:
-    #         pyglet.app.exit()
-    #     else:
-    #         print('key code '+str(symbol))
-
-    # def on_key_release(self, symbol, modifiers):
-    #     if symbol == key.UP:
-    #         print('release accelerate')
-    #     elif symbol == key.DOWN:
-    #         print('release reverse')
-    #     elif symbol == key.LEFT:
-    #         print('release turn left')
-    #     elif symbol == key.RIGHT:
-    #         print('release turn right')
-    #     else:
-    #         print('release key code '+str(symbol))
 
     def on_draw(self):
         self.clear()
@@ -88,12 +71,33 @@ class MyWindow(pyglet.window.Window):
     def update(self, dt):
         if self.key_handler[key.LEFT]:
             print('turn left')
-            #self.car.updateY(20)
-            self.car.updateTheta(-2)
+            self.car.updateTheta(-self.car.rotation_speed* dt)
         if self.key_handler[key.RIGHT]:
             print('turn right')
-            #self.car.updateY(-20)
-            self.car.updateTheta(2)
+            self.car.updateTheta(self.car.rotation_speed* dt)
+        if self.key_handler[key.UP]:
+            thetaRadian = -math.radians(self.car.getTheta())
+            deltaX = math.cos(thetaRadian) * self.car.speed * dt
+            deltaY = math.sin(thetaRadian) * self.car.speed * dt
+            self.car.updateX(deltaX*1.1)
+            self.car.updateY(deltaY*1.1)
+            self.car.updateDeltaX(deltaX)
+            self.car.updateDeltaY(deltaY)
+            print(str(self.car.getDeltaX())+' '+str(self.car.getDeltaY()))
+
+        if self.key_handler[key.DOWN]:
+            thetaRadian = -math.radians(self.car.getTheta())
+            deltaX = math.cos(thetaRadian) * self.car.speed * dt
+            deltaY = math.sin(thetaRadian) * self.car.speed * dt
+            self.car.updateX(-deltaX)
+            self.car.updateY(-deltaY)
+            self.car.updateDeltaX(-deltaX)
+            self.car.updateDeltaY(-deltaY)  
+
+        self.car.updateX(self.car.getDeltaX())
+        self.car.updateY(self.car.getDeltaY())
+        self.car.updateDeltaX(self.car.getDeltaX() * 0.95)
+        self.car.updateDeltaY(self.car.getDeltaY() * 0.95)
 
 if __name__ == "__main__":
     window = MyWindow(WINDOWWIDTH,WINDOWHEIGHT, "DRIFT AI", resizable=True, vsync =True)
